2b5d765bc723ee427bc9411b4f3a13d451c040c2
Nick Paolino <github email address>
2017-11-06 09:36:18 -0500

    Finished

--- oo-index.js
+++ oo-index.js
@@ -1,3 +1,5 @@
+var eventArea = document.getElementById('event_area');
+
 class Unicorn {
   constructor(name, occupation) {
     this.name = name;
@@ -5,4 +7,57 @@ class Unicorn {
     this.health = 1000
   }
 
+  attack(){
+    this.createDiv("'ATTAAAAAAAAAK!''");
+    this.changeImage('attack');
+  }
+
+  defend(){
+    this.createDiv("You put sunglasses on.");
+    this.changeImage("defend");
+  }
+
+  heal(){
+    this.createDiv("You feel refreshed after yoga.");
+    this.changeImage("heal");
+  }
+
+  breakdance(){
+    this.createDiv("It's really spinning class");
+    this.changeImage("breakdance");
+  }
+
+  eatCronut(){
+    this.createDiv("You've gained 1000 health points.");
+    this.changeImage("eat");
+  }
+
+  restart(){
+    while (eventArea.hasChildNodes()){
+      eventArea.removeChild(eventArea.lastChild);
+    }
+    this.createDiv("Out on the street, ready to go!");
+    this.changeImage("new");
+  }
+
+  createDiv(text){
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode(text));
+    eventArea.prepend(div);
+  }
+
+  changeImage(className){
+    const imageArea = document.getElementById('image_area');
+    imageArea.className = className;
+  }
+
 }
+
+const unicorn = new Unicorn("New", "Unicorn");
+
+document.getElementById('restart').addEventListener("click",()=> unicorn.restart())
+document.getElementById('attack').addEventListener("click",()=> unicorn.attack())
+document.getElementById('defend').addEventListener("click",()=> unicorn.defend())
+document.getElementById('heal').addEventListener("click",()=> unicorn.heal())
+document.getElementById('breakdance').addEventListener("click",()=> unicorn.breakdance())
+document.getElementById('eat').addEventListener("click",()=> unicorn.eatCronut())
